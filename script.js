3d7f0332fc4a3977cc6dcc10333a2a090c637bcc
bon nocos <bon.nocos@gmail.com>
2022-07-20 17:43:31 +0800

    add books functionality, and ressetting of forms after submit

--- script.js
+++ script.js
@@ -5,26 +5,32 @@ let isRead = document.querySelector('#isRead');
 let submit = document.querySelector('.submit');
 let add = document.querySelector('#add');
 let container = document.querySelector('.book-container')
+let form = document.querySelector('#form')
 
 // Btn Event Listener
 submit.addEventListener('click', addBookToLibrary)
 
 
 // Library Array
-let myLibrary = [
-    {
-        title: 'The Lord of the Rings: The Fellowship of the Ring',
-        author: 'J.R.R Tolkien',
-        pages: 423,
-        isRead: 'yes'
-    },
-    {
-        title: 'The Lord of the Rings: The Two Towers',
-        author: 'J.R.R Tolkien',
-        pages: 352,
-        isRead: 'no'
-    }
-]
+let myLibrary = []   // {
+    //     title: 'The Lord of the Rings: The Fellowship of the Ring',
+    //     author: 'J.R.R Tolkien',
+    //     pages: 423,
+    //     isRead: 'yes'
+    // },
+    // {
+    //     title: 'The Lord of the Rings: The Two Towers',
+    //     author: 'J.R.R Tolkien',
+    //     pages: 352,
+    //     isRead: 'no'/
+    // },
+    // {
+    //     title: 'The lord of the Rings: The Return of the King',
+    //     author: 'J.R.R Tolkien',
+    //     pages: 416,
+    //     isRead: 'no'
+    // }
+
 
 // Object Constructor
 function Book(title, author, pages, isRead) {
@@ -34,21 +40,30 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead
 }
 
+// Function to display books
+function displayBooks() {
+    myLibrary.forEach(book => {
+        const card = document.createElement('div')
+        card.innerText = book.title
+    
+        container.append(card)
+    })
+}
+
 // Function to add book to the Library
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    e.preventDefault()
     title = title.value
     author = author.value
     pages = pages.value
 
-    isRead.checked ? isRead = isRead.value = 'yes' : isRead = isRead.value = 'no'
+    isRead.checked ? isRead = isRead.value = 'yes' : isRead = isRead.value = 'no';
+
     const newBook = new Book(title, author, pages, isRead)
     myLibrary.push(newBook);
+    console.log(myLibrary, newBook)
+    displayBooks();
+    form.reset()
 }
 
-// Function to display books
-myLibrary.forEach(book => {
-    const card = document.createElement('div')
-    card.innerText = book.title
-
-    container.append(card)
-})
+displayBooks();
